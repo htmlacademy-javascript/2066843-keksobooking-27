d59280da27f5d3016950d653d74590494c66a6e8
Stanislav Prokofjev <Prokostas@gmail.com>
2022-09-29 23:03:02 +0300

    first random function

---
+++ js/main.js
@@ -0,0 +1,18 @@
+let getRandom = function(min, max, floatNumber){
+  if(min < 0 || max < 0 || floatNumber < 0){
+    return NaN
+  } else if (min > max){
+    let c = min;
+    min = max;
+    max = c;
+  } else if (min == max){
+    return min;
+  } else {
+  }
+
+  const randomNumbder = (Math.random() * (max - min) + min).toFixed(floatNumber);
+  return parseFloat(randomNumbder);
+}
+
+let result = getRandom(5, 40, 10);
+console.log(result);
