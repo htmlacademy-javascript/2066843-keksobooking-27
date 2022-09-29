92ad85812dd037099cd694acfa272e198f60c65f
Keks <keks@htmlacademy.ru>
2022-09-29 23:16:15 +0300

    :heavy_check_mark: Сборка #2

---
+++ 2/js/main.js
@@ -0,0 +1,16 @@
+const getRandom = function(min, max, floatNumber){
+  if(min < 0 || max < 0 || floatNumber < 0){
+    return NaN;
+  } else if (min > max){
+    const c = min;
+    min = max;
+    max = c;
+  } else if (min === max){
+    return min;
+  }
+
+  const randomNumbder = (Math.random() * (max - min) + min).toFixed(floatNumber);
+  return parseFloat(randomNumbder);
+};
+
+getRandom(2, 120, 2);
