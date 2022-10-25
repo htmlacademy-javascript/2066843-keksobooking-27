b27da4e5d50014c8776fa199bfabf22455ea2d49
Keks <keks@htmlacademy.ru>
2022-10-25 22:42:37 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/main.js
@@ -0,0 +1,7 @@
+import './element.js';
+import {pageDisabled} from './form.js';
+import './map.js';
+import { loadMap } from './map.js';
+
+pageDisabled();
+loadMap();
---
+++ 8/js/map.js
@@ -0,0 +1,82 @@
+import {createObjects} from './data.js';
+import { getCard } from './element.js';
+import { pageActive } from './form.js';
+
+const randomObjects = createObjects(10);
+const address = document.querySelector('#address');
+const TOKYO_COORDINATES = {
+  lat: 35.672855,
+  lng: 139.817413,
+};
+
+const map = L.map('map-canvas')
+  .setView({
+    lat: TOKYO_COORDINATES.lat,
+    lng: TOKYO_COORDINATES.lng,
+  }, 10);
+
+L.tileLayer(
+  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  },
+).addTo(map);
+
+//main marker
+const mainPinIcon = L.icon({
+  iconUrl: '../img/main-pin.svg',
+  iconSize: [52, 52],
+  iconAnchor: [26, 52],
+});
+
+const mainMarker = L.marker(
+  {
+    lat: TOKYO_COORDINATES.lat,
+    lng: TOKYO_COORDINATES.lng,
+  },
+  {
+    draggable: true,
+    icon: mainPinIcon,
+  },
+);
+
+mainMarker.addTo(map);
+
+//similar other markers
+const similarPinIcon = L.icon({
+  iconUrl: '../img/pin.svg',
+  iconSize: [40, 40],
+  iconAnchor: [20, 40],
+});
+
+
+randomObjects.forEach((element) =>{
+  const lat = element.location.lat;
+  const lng = element.location.lng;
+  const marker = L.marker({
+    lat,
+    lng,
+  },{
+    icon: similarPinIcon,
+  });
+
+  marker
+    .addTo(map)
+    .bindPopup(getCard(element));
+});
+
+const getCoordinates = function(element) {
+  address.value = `${element.lat}, ${element.lng}`;
+};
+
+mainMarker.on('moveend', (evt) => {
+  const mainMarkerCoordinates = evt.target.getLatLng();
+  getCoordinates(mainMarkerCoordinates);
+});
+
+const loadMap = () => {
+  map.on('load', pageActive());
+  getCoordinates(TOKYO_COORDINATES);
+};
+
+export {loadMap};
