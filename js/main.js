77dd136271c30df6fee9d862ceff73bc000b46d6
Stanislav Prokofjev <Prokostas@gmail.com>
2022-10-27 23:37:37 +0300

    fix map file import

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 import { getData } from './api.js';
 import './element.js';
 import {pageDisabled, setUserFormSubmit} from './form.js';
-import './map.js';
 import { loadMap, putOnMap } from './map.js';
 import { resetForm } from './util.js';
 
